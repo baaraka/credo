f13806ddae5c7960d70c9a4192ab002b4099faf0
barakaganai <barakaganai01@gmail.com>
2024-08-02 12:47:36 +0300

    create mainLayout

---
+++ src/layouts/MainLayouts.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <Navbar :class="{ 'bg-scrolled': isScrolled }" />
+    <main ref="content" class="flex-grow overflow-y-auto" @scroll="onScroll">
+      <router-view />
+    </main>
+    <FooterComponent />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Navbar from "@/components/navigation/Navbar.vue";
+import FooterComponent from "@/components/navigation/FooterComponent.vue";
+
+//track scroll state
+const isScrolled = ref(false);
+const content = ref(null);
+
+//function to handle scroll events
+const onScroll = () => {
+  if (content.value.scrollTop > 0) {
+    isScrolled.value = true;
+  } else {
+    isScrolled.value = false;
+  }
+};
+</script>
