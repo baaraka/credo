523be082a681220001e01414fdb1ab8c113d3e5e
barakaganai <barakaganai01@gmail.com>
2024-08-02 12:48:48 +0300

    create categories page

---
+++ src/components/home/Categories.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="">
+    <div class="flex items-center gap-4 mb-6">
+      <i class="pi pi-bars text-2xl text-[#3b5998]"></i>
+      <h1 class="text-2xl font-bold text-[#3b5998]">Categories</h1>
+    </div>
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-5">
+      <div
+        class="flex flex-col items-center gap-5 border bg-white p-4 rounded-3xl shadow-md cursor-pointer"
+      >
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKyPAU4KUrOTW4Zh70mW71y5Z0ZbHeQVfLcA&s"
+          alt="Nothing phone"
+          class="rounded-lg object-cover"
+        />
+        <h2 class="text-base font-medium">Smart Phone</h2>
+      </div>
+      <div
+        class="flex flex-col items-center gap-5 border bg-white p-4 rounded-3xl shadow-md cursor-pointer"
+      >
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKyPAU4KUrOTW4Zh70mW71y5Z0ZbHeQVfLcA&s"
+          alt="Nothing phone"
+          class="rounded-lg object-cover"
+        />
+        <h2 class="text-base font-medium">Feature Phone</h2>
+      </div>
+      <div
+        class="flex flex-col items-center gap-5 border bg-white p-4 rounded-3xl shadow-md cursor-pointer"
+      >
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKyPAU4KUrOTW4Zh70mW71y5Z0ZbHeQVfLcA&s"
+          alt="Nothing phone"
+          class="rounded-lg object-cover"
+        />
+        <h2 class="text-base font-medium">Smart Tv</h2>
+      </div>
+      <div
+        class="flex flex-col items-center gap-5 border bg-white p-4 rounded-3xl shadow-md cursor-pointer"
+      >
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKyPAU4KUrOTW4Zh70mW71y5Z0ZbHeQVfLcA&s"
+          alt="Nothing phone"
+          class="rounded-lg object-cover"
+        />
+        <h2 class="text-base font-medium">Air Condition</h2>
+      </div>
+    </div>
+  </div>
+</template>
