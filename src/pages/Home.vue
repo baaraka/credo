e96e0e9d75e38b44e6187769cb2b0567b50b15d0
barakaganai <barakaganai01@gmail.com>
2024-08-02 12:46:44 +0300

    create home page

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Feature from "@/components/home/Feature.vue";
+import Categories from "@/components/home/Categories.vue";
+import slider from "@/components/home/Slider.vue";
+</script>
+
+<template>
+  <div class="px-6 py-10">
+    <slider />
+    <Feature />
+    <Categories />
+  </div>
+</template>
