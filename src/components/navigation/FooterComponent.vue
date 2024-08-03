9dde3fa2854e71d469ecef81d24ae27738b23db9
barakaganai <barakaganai01@gmail.com>
2024-08-03 09:26:50 +0300

    add isActive functionality

--- src/components/navigation/FooterComponent.vue
+++ src/components/navigation/FooterComponent.vue
@@ -2,31 +2,76 @@
   <footer class="grid grid-cols-4 px-2">
     <router-link
       to="/"
-      class="flex flex-col gap-2 items-center cursor-pointer py-2 hover:bg-[#e0e5ee] hover:rounded-md"
+      :class="[
+        isLinkActive('/') ? 'bg-[#e0e5ee]' : 'hover:bg-[#f4f5f6]',
+        'flex',
+        'flex-col',
+        'gap-2',
+        'items-center',
+        'cursor-pointer',
+        'py-2',
+        'rounded-md',
+      ]"
     >
       <i class="pi pi-home text-xl"></i>
       <span>Home</span>
     </router-link>
     <router-link
       to="/categories"
-      class="flex flex-col gap-2 items-center cursor-pointer py-2 hover:bg-[#e0e5ee] hover:rounded-md"
+      :class="[
+        isLinkActive('/categories') ? 'bg-[#e0e5ee]' : 'hover:bg-[#f4f5f6]',
+        'flex',
+        'flex-col',
+        'gap-2',
+        'items-center',
+        'cursor-pointer',
+        'py-2',
+        'rounded-md',
+      ]"
     >
       <i class="pi pi-clone text-xl"></i>
       <span>Categories</span>
     </router-link>
     <router-link
       to="/chats"
-      class="flex flex-col gap-2 items-center py-2 hover:bg-[#e0e5ee] hover:rounded-md"
+      :class="[
+        isLinkActive('/chats') ? 'bg-[#e0e5ee]' : 'hover:bg-[#f4f5f6]',
+        'flex',
+        'flex-col',
+        'gap-2',
+        'items-center',
+        'cursor-pointer',
+        'py-2',
+        'rounded-md',
+      ]"
     >
       <i class="pi pi-envelope text-xl"></i>
       <span>Message</span>
     </router-link>
     <router-link
       to="/signIn"
-      class="flex flex-col gap-2 items-center py-2 hover:bg-[#e0e5ee] hover:rounded-md"
+      :class="[
+        isLinkActive('/signIn') ? 'bg-[#e0e5ee]' : 'hover:bg-[#f4f5f6]',
+        'flex',
+        'flex-col',
+        'gap-2',
+        'items-center',
+        'cursor-pointer',
+        'py-2',
+        'rounded-md',
+      ]"
     >
       <i class="pi pi-sign-in text-xl"></i>
       <span>Sign In</span>
     </router-link>
   </footer>
 </template>
+
+<script setup>
+import { useRoute } from "vue-router";
+
+const isLinkActive = (routePath) => {
+  const route = useRoute();
+  return route.path === routePath;
+};
+</script>
